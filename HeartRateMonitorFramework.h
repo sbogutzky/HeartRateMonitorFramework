5a38753592f75c32095ac7c9c4f4a2a93ab10cb1
Simon Bogutzky <simon.bogutzky@t-online.de>
2014-10-17 12:38:44 +0200

    Framework hinzugefügt

---
+++ HeartRateMonitorFramework.h
@@ -0,0 +1,25 @@
+//
+//  HeartRateMonitorFramework.h
+//  HeartRateMonitorFramework
+//
+//  Created by Simon Bogutzky on 18.06.14.
+//  Copyright (c) 2014 Simon Bogutzky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HeartRateMonitorFramework.
+FOUNDATION_EXPORT double HeartRateMonitorFrameworkVersionNumber;
+
+//! Project version string for HeartRateMonitorFramework.
+FOUNDATION_EXPORT const unsigned char HeartRateMonitorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeartRateMonitorFramework/PublicHeader.h>
+
+#import <HeartRateMonitorFramework/HeartRateMonitorManager.h>
+#import <HeartRateMonitorFramework/HeartRateMonitorManagerDelegate.h>
+#import <HeartRateMonitorFramework/HeartRateMonitorDevice.h>
+#import <HeartRateMonitorFramework/HeartRateMonitorDeviceDelegate.h>
+#import <HeartRateMonitorFramework/HeartRateMonitorData.h>
+
+
